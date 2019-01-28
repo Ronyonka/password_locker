13e216b21de755ef59814d10d54d6f6ea8ca2705
Ronyonka <rononyonka@gmail.com>
2019-01-28 05:40:11 +0300

    added instances for credentials

---
+++ credentials.py
@@ -0,0 +1,8 @@
+class Credentials:
+    """
+     Class that generates new instances of credentials
+    """
+    credentials_list = []
+    def __init__(self, account_name, account_password):
+        self.account_name = account_name
+        self.account_password = account_password
