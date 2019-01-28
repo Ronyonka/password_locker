6e6a1494f7edfc42b8341aade08bbd0b440ff5f3
Ronyonka <rononyonka@gmail.com>
2019-01-28 09:55:19 +0300

    credentials test

--- credentials_test.py
+++ credentials_test.py
@@ -8,16 +8,22 @@ class TestCredentials(unittest.TestCase):
 
     def setUp(self):
 
-        self.new_credential = Credentials('Twitter','ron_onyonka','password3')
+        self.new_credentials = Credentials('Twitter','ron_onyonka','password3')
 
     def test_init(self):
         """
         test_init test case to test if the object is initialized properly
         """
-        self.assertEqual(self.new_credential.account, "Twitter")
-        self.assertEqual(self.new_credential.account_name, "ron_onyonka")
-        self.assertEqual(self.new_credential.account_password, "password3")
+        self.assertEqual(self.new_credentials.account, "Twitter")
+        self.assertEqual(self.new_credentials.account_name, "ron_onyonka")
+        self.assertEqual(self.new_credentials.account_password, "password3")
 
+    def test_save_credentials(self):
+        '''
+        test case to test if the credentials objects are saved into the contact list
+        '''
+        self.new_credentials.test_save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),1)
 
 if __name__ == '__main__':
     unittest.main()
